9bb963cf75f0babcb184e472e6b952c9075ef0fc
Lucas Rival <lucas.rival@gmail.com>
2018-10-17 16:53:50 +0200

    add final design to webUI

--- app.py
+++ app.py
@@ -4,12 +4,13 @@
 from flask import Flask
 from flask import send_from_directory
 from flask import render_template
-import multiprocessing
-
+from flask import jsonify
 from flask_bootstrap import Bootstrap
 from flask_bootstrap import WebCDN
 from SWProxy import SWProxyStart
 from os import listdir
+import multiprocessing
+import socket
 
 
 app = Flask(__name__)
@@ -19,16 +20,32 @@ app.extensions['bootstrap']['cdns']['bootstrap'] = WebCDN('//cdnjs.cloudflare.co
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    dloads_dir = 'data'
-    dloads = listdir(dloads_dir)
-    dloads_src = ['/download/{}'.format(i) for i in dloads]
-    return render_template('index.html', title='Welcome SWProxy Web App', dloads=dloads, dloads_src=dloads_src, ip=ip, port=port)
+    return render_template('index.html', title='Welcome SWProxy Web App')
 
 
 @app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html', title='About')
 
+@app.route('/start', methods=['GET'])
+def start():
+    port = 8080
+    ip = [[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]][0]
+    log = 'INFO'
+    SWProxy = multiprocessing.Process(target=SWProxyStart, args=(ip, port, log))
+    SWProxy.start()
+    if (SWProxy.is_alive()):
+        return render_template('start.html', title='SWProxy on progress', status= 'sucess', ip=ip, port=port)
+    else:
+        return render_template('start.html', title='Failed to launch', status= 'failed')
+
+
+@app.route('/files', methods=['GET'])
+def files():
+    dloads_dir = 'data'
+    dloads = listdir(dloads_dir)
+    dloads_src = ['/download/{}'.format(i) for i in dloads]
+    return jsonify(dloads_src)
 
 @app.route('/download/<path:filename>', methods=['GET', 'POST'])
 def download(filename):
@@ -43,11 +60,5 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-
-    port = 8080
-    ip = '192.168.1.21'
-    log = 'INFO'
-    SWProxy = multiprocessing.Process(target=SWProxyStart, args=(ip, port, log))
-    SWProxy.start()
     app.run(host="0.0.0.0", port=80)
 
